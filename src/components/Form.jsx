7704bc47335a0b4a2ecfe96662ada130577f1c01
emersondemetrio <emer.demetrio@gmail.com>
2019-06-17 23:02:52 -0300

    canAdd; remove

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import uuidv4 from 'uuid';
 
 class Form extends React.Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
+			canSend: false,
 			preAnswer: '',
 			answers: []
 		}
@@ -12,12 +14,16 @@ class Form extends React.Component {
 
 	addAnswer = () => {
 		this.setState(({
+			canSend: true,
 			preAnswer: '',
 			answers: [
 				...this.state.answers,
-				this.state.preAnswer
+				{
+					id: uuidv4(),
+					name: this.state.preAnswer
+				}
 			]
-		}))
+		}), () => console.log(this.state, this.state.answers));
 	}
 
 	preAdd = (pre) => {
@@ -25,6 +31,20 @@ class Form extends React.Component {
 			...state,
 			preAnswer: pre
 		}));
+	}
+
+	removeAnswer = id => {
+		this.setState(state => ({
+			...state,
+			answers: this.state.answers.filter(a => a.id !== id)
+		}), () => {
+			if (this.state.answers.length < 1) {
+				this.setState(state => ({
+					...state,
+					canSend: false
+				}));
+			}
+		});
 	}
 
 	render() {
@@ -39,8 +59,17 @@ class Form extends React.Component {
 				</button>
 
 				<ul>
-					{this.state.answers.map((a, i) => <li key={i}>{a}</li>)}
+					{this.state.answers.map(a =>
+						<li key={a.id}>
+							{a.name}
+							<button onClick={_ => this.removeAnswer(a.id)}>x</button>
+						</li>
+					)}
 				</ul>
+
+				<button disabled={!this.state.canSend} type="submit">
+					Send
+				</button>
 			</div>
 		);
 	}
